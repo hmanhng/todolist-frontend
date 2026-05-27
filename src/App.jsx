de1a9e80fcd52340c778c9dba7b78dbf997fa95e
hmanhng <hmanhng@icloud.com>
2026-05-27 09:26:58 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -101,7 +101,7 @@ function App() {
           type="text"
           value={newTodo}
           onChange={(e) => setNewTodo(e.target.value)}
-          placeholder="hello world"
+          placeholder="abcxyz"
           style={styles.input}
         />
         <button type="submit" style={styles.addButton}>Add</button>
